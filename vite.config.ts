ba31e0e3b884b3e13a2646d578437e9b7260ddc7
lloydlobo <lloyd@lloydlobo.com>
2022-07-27 14:30:12 +0530

    test: install and create vitest test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  test: {
+    exclude: [...configDefaults.exclude, 'packages/template/*', 'tests/*'],
+    environment: 'happy-dom',
+  },
 });
