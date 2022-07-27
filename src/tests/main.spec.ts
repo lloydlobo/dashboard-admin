3512353398da6727b7721f827b2498517808f142
lloydlobo <lloyd@lloydlobo.com>
2022-07-27 14:44:10 +0530

    test: add happy dom mock app test

--- src/tests/main.spec.ts
+++ src/tests/main.spec.ts
@@ -1,7 +1,29 @@
 import { describe, expect, it } from 'vitest';
+import App from '../App.svelte';
+
+function appendDivIdApp() {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'app');
+  document.body.appendChild(div);
+}
+
+function createApp() {
+  appendDivIdApp();
+  return new App({
+    target: document.getElementById('app'),
+  });
+}
 
 describe('should work', () => {
   it('should work', () => {
     expect(true).toBeTruthy();
+  });
+});
+
+describe('app should work', () => {
+  // @vitest-environment happy-dom
+  it('should work', () => {
+    const app = createApp();
+    expect(app).toBeDefined();
   });
 });
