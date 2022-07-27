43f20a6577a8ee5435c9f33937b7b9f7280cfce5
lloydlobo <lloyd@lloydlobo.com>
2022-07-27 19:16:55 +0530

    test: add unit test for arrayLinks

---
+++ src/lib/data/links.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+
+import { arrayLinks } from './links';
+
+function appendDivIdApp() {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'app');
+  document.body.appendChild(div);
+}
+
+// function createApp() {
+//   appendDivIdApp();
+//   return new App({
+//     target: document.getElementById('app'),
+//   });
+// }
+
+describe('arrayLinks should work', () => {
+  // @vitest-environment happy-dom
+  it('arrayLinks is defined', () => {
+    const links = arrayLinks;
+    expect(links).toBeDefined();
+  });
+
+  it('arrayLinks length is 6', () => {
+    expect(arrayLinks.length).toBe(6);
+  });
+});
