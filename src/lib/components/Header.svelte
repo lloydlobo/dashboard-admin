1b07100e04b88792fd2d926e19db2e671f086159
lloydlobo <lloyd@lloydlobo.com>
2022-07-28 11:20:19 +0530

    feat: destructure user avatar & name objects

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-  const avatarSvg = `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M16 2l5 5v14.008a.993.993 0 0 1-.993.992H3.993A1 1 0 0 1 3 21.008V2.992C3 2.444 3.445 2 3.993 2H16zm-4 9.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zM7.527 17h8.946a4.5 4.5 0 0 0-8.946 0z"></path></g></svg>`;
-
   const user = {
-    name: {
-      first_name: 'Morgan',
-      middle_name: '',
-      last_name: 'Oakley',
-    },
     username: 'morgan',
+    name: {
+      FIRST_NAME: 'Morgan',
+      MIDDLE_NAME: '',
+      LAST_NAME: 'Oakley',
+    },
+    avatar: `<svg
+      stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M16 2l5 5v14.008a.993.993 0 0 1-.993.992H3.993A1 1 0 0 1 3 21.008V2.992C3 2.444 3.445 2 3.993 2H16zm-4 9.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zM7.527 17h8.946a4.5 4.5 0 0 0-8.946 0z"></path></g></svg>`,
   };
 
-  const first_name = user.name.first_name;
-  const middle_name = user.name.middle_name;
-  const last_name = user.name.last_name;
+  const { avatar, username } = user;
+  const { FIRST_NAME, MIDDLE_NAME, LAST_NAME } = user.name;
 </script>
 
 <header>
@@ -23,9 +22,9 @@
           <div
             class="avatar-bg relative flex rounded-full bg-yellow-400 object-contain"
           >
-            <a href={user.username} alt={`@${user.username}`}
+            <a href={username} alt={`@${username}`}
               ><div class="avatar-svg min-h-full p-2">
-                {@html avatarSvg}
+                {@html avatar}
               </div></a
             >
           </div>
@@ -35,11 +34,11 @@
         <span class="greeting text-base font-bold">Hi there,</span>
         <div class="user flex gap-x-2 font-bold">
           <span class="user-fullname">
-            {first_name}
-            {middle_name}
-            {last_name}
+            {FIRST_NAME}
+            {MIDDLE_NAME}
+            {LAST_NAME}
           </span>
-          <span class="user-username">(@{user.username})</span>
+          <span class="user-username">(@{username})</span>
         </div>
       </div>
     </div>
--- src/lib/data/links.spec.ts
+++ src/lib/data/links.spec.ts
@@ -2,11 +2,11 @@ import { describe, expect, it } from 'vitest';
 
 import { arraySettings, arrayLinks } from './links';
 
-function appendDivIdApp() {
-  const div = document.createElement('div');
-  div.setAttribute('id', 'app');
-  document.body.appendChild(div);
-}
+// function appendDivIdApp() {
+//   const div = document.createElement('div');
+//   div.setAttribute('id', 'app');
+//   document.body.appendChild(div);
+// }
 
 // function createApp() {
 //   appendDivIdApp();
@@ -24,5 +24,17 @@ describe('arrayLinks should work', () => {
 
   it('arrayLinks length is 6', () => {
     expect(arrayLinks.length).toBe(6);
+  });
+});
+
+describe('arraySettings should work', () => {
+  // @vitest-environment happy-dom
+  it('arraySettings is defined', () => {
+    const links = arraySettings;
+    expect(links).toBeDefined();
+  });
+
+  it('arraySettings length is 6', () => {
+    expect(arraySettings.length).toBe(3);
   });
 });
