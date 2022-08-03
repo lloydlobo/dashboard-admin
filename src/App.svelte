33ea96af8e753b53308d270b77c6ef7e00d4fcc8
Lloyd Lobo <lloyd@lloydlobo.com>
2022-08-03 19:00:42 +0530

    ci: update playwright peer-deps

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,6 @@
   import Main from './lib/layout/Main.svelte';
   import Sidebar from './lib/layout/Sidebar.svelte';
   import Footer from './lib/layout/Footer.svelte';
-  import { projects } from './lib/stores/projectsStore';
-
-  const arrayData = [];
-  arrayData.push(projects);
 </script>
 
 <Header />
@@ -15,29 +11,3 @@
 <Main />
 <Sidebar />
 <Footer />
-
-<style>
-  /* .design-bg-img { */
-
-  /*   top: 0; */
-
-  /*   right: 0; */
-
-  /*   left: 0; */
-
-  /*   height: 100vh; */
-
-  /*   background-image: url('https://cdn.statically.io/gh/TheOdinProject/curriculum/main/html_css/grid-lessons/project-dashboard/dashboard-project.png'); */
-
-  /*   background-repeat: no-repeat; */
-
-  /*   background-attachment: fixed; */
-
-  /*   background-position: center center; */
-
-  /*   background-size: contain; */
-
-  /* } */
-
-  /*  https://css-tricks.com/perfect-full-page-background-image/ */
-</style>
