b8fb5d70f510dc441d96f47c74d88c3df77d2628
Lloyd Lobo <lloyd@lloydlobo.com>
2022-08-03 16:55:16 +0530

    feat: add project action icons in card

--- src/lib/components/ProjectIcons.svelte
+++ src/lib/components/ProjectIcons.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
-  export let completed: boolean;
-  export let id: number;
+  import SvgStarPlus from '../../../public/star-plus.svg';
+  import SvgEyePlus from '../../../public/eye-plus-outline.svg';
+  import SvgShare from '../../../public/share-variant.svg';
 </script>
 
-<div class="icons actions flex items-end justify-end gap-4">
-  <span class="cursor-pointer font-bold hover:shadow-md"> {id}</span>
-  <div
-    class="status
-  cursor-pointer
-  rounded
-  font-bold
-  hover:shadow-md"
-  >
-    {completed}
-  </div>
+<div class="project-actions flex h-6 justify-end gap-4">
+  <img class="project-action-svg" src={SvgStarPlus} alt="Favorite it" />
+  <img class="project-action-svg" src={SvgEyePlus} alt="Watch this" />
+  <img class="project-action-svg" src={SvgShare} alt="Share Project" />
 </div>
+
+<style>
+  img.project-action-svg {
+    @apply shadow-none duration-300 ease-in;
+  }
+
+  img.project-action-svg:hover {
+    @apply -translate-y-1 shadow-xl duration-300 ease-in-out;
+  }
+</style>
--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -4,26 +4,19 @@
 </script>
 
 <div class="projects-cards grid grid-flow-col gap-8">
-  {#each projects as { userId, id, title, completed }}
+  {#each projects as { userId, title }}
     <div
-      class=" project
-      grid
-      cursor-pointer
-      break-words
-      rounded-lg
-      border-l-4
-      border-amber-300
-      bg-white
-      p-4
-      leading-snug
-      shadow-md
-      duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl "
+      class="
+      project
+      grid cursor-pointer break-words rounded-lg border-l-4 border-amber-300 bg-white p-4 leading-snug shadow-md duration-300 ease-in-out hover:-translate-y-1 hover:shadow-2xl "
     >
-      <h3 class="project-title text-xl font-bold uppercase">{title}</h3>
+      <h3 class=" project-title text-xl font-bold uppercase">
+        {title}
+      </h3>
       <p class="project-title ">{title + title + title}</p>
       <p class="projectId underline">{userId} {new Date().getFullYear()}</p>
 
-      <ProjectIcons {completed} {id} />
+      <ProjectIcons />
     </div>
   {/each}
 </div>
